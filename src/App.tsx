1f71435c662b2a0c6c91ec3444bb82398f2e2a3e
penny <pennyjlynette@gmail.com>
2024-10-23 10:31:49 -0400

    redo routes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/num" element={<NumberLineGame />} />
+        <Route path="/" element={<NumberLineGame />} />
         <Route path="/mach" element={<FunctionMachine />} />
         <Route path="/place" element={<PlaceValueBlocks />}  />
         <Route path="/deriv" element={<DerivativeSuite />} />
